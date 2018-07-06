ce955850870c4f545556a66174e23eb14dd0c543
maiquocqui <maiquocqui@gmail.com>
2018-07-06 13:19:45 +0700

    ...

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var babel = require('gulp-babel');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 var imagemin = require('gulp-imagemin');
-var sitePort = 8107;
+var sitePort = 8108;
 // Browser Sync
 
 // ConCat
